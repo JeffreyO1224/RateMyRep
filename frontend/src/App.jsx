740e9f6e8de081d80c3820a0f869096ca2f95592
Jeffrey Oyuela <jeffreysmac@Jeffreys-MacBook-Pro.local>
2025-05-02 16:33:23 -0400

    Added page files that don't really do much

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,39 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+import HomePage from './pages/Homepage/Homepage';
+import LoginPage from './pages/LoginPage/LoginPage'
+import ErrorPage from './pages/ErrorPage/ErrorPage'
+import SignupPage from './pages/SignupPage/SignupPage'
+import './App.css';
+
 
 function App() {
 
-  return (
-    <>
-    </>
-  )
+  const router = createBrowserRouter([
+      {
+        path: '/',
+        errorElement: <ErrorPage />,
+        children: [
+          { 
+            path: '/',
+            element: <HomePage /> 
+          },
+          { 
+            path: '/login/', 
+            element: <LoginPage /> 
+          },
+          {
+            path: '/signup',
+            element: <SignupPage />
+          }
+        ]
+      }
+    ]);
+  
+    return <RouterProvider router={router} />;
 }
 
 export default App
