bde7f5681f6bd897eca344f34c0f88f1411f19cb
Emily Lin <linemily62@gmail.com>
2025-05-03 13:23:16 -0400

    cosponsor work

--- frontend/src/BillsPage.jsx
+++ frontend/src/BillsPage.jsx
@@ -3,10 +3,11 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 const BillsPage = () => {
   const { bioguideId } = useParams();
-  const [bills, setBills] = useState([]);
+  const [sponsorBills, setSponsorBills] = useState([]);
+  const [cosponsorBills, setCosponsorBills] = useState([]);
   const apiKey = import.meta.env.VITE_REACT_APP_CONGRESS_API_KEY;
   useEffect(() => {
-    const fetchBills = async () => {
+    const fetchSponsorBills = async () => {
       try {
         const res = await axios.get(
           `https://api.congress.gov/v3/member/${bioguideId}/sponsored-legislation`,
@@ -16,20 +17,54 @@ const BillsPage = () => {
             },
           }
         );
-        setBills(res.data.sponsoredLegislation || []);
+        setSponsorBills(res.data.sponsoredLegislation || []);
       } catch (error) {
-        console.error('Error fetching bills:', error);
+        console.error('Error fetching sponsor bills:', error);
       }
     };
 
-    fetchBills();
+    fetchSponsorBills();
   }, [bioguideId]);
+
+  useEffect(() => {
+    const fetchCosponsorBills = async () => {
+      try {
+        const res = await axios.get(
+          `https://api.congress.gov/v3/member/${bioguideId}/cosponsored-legislation`,
+          {
+            params: {
+              api_key: apiKey,
+            },
+          }
+        );
+        setCosponsorBills(res.data.cosponsoredLegislation || []);
+      } catch (error) {
+        console.error('Error fetching cosponsor bills:', error);
+      }
+    };
+
+    fetchCosponsorBills();
+  }, [bioguideId]);
+
+
 
   return (
     <div>
       <h2>Sponsored Bills</h2>
       <ul>
-        {bills.map((bill, index) => (
+        {sponsorBills.map((bill, index) => (
+          <li key={index}>
+            <a href={bill.url} target="_blank" rel="noopener noreferrer">
+              {bill.title} ({bill.congress}th Congress)
+            </a>
+            <p><strong>Policy Area:</strong> {bill.policyArea?.name}</p>
+            <p><strong>Latest Action:</strong> {bill.latestAction?.text}</p>
+          </li>
+        ))}
+      </ul>
+      <h2>Cosponsored Bills</h2>
+      <ul>
+        {cosponsorBills.map((bill, index) => (
           <li key={index}>
             <a href={bill.url} target="_blank" rel="noopener noreferrer">
               {bill.title} ({bill.congress}th Congress)
