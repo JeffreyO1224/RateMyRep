bff0e55ca5a06b6462f72010aeb90ed334eed003
Emily Lin <emilylin@Emilys-MacBook-Air-3.local>
2025-05-02 20:56:01 -0400

    geocoding works, gmaps api set up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import React from "react";
+import Geocode from "./Geocode.jsx";
+
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="App">
+      <Geocode />
+    </div>
   )
 }
 
