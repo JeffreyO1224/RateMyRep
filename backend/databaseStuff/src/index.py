8e3be479c487facddc7dcabfb81f450dd5480ad7
Jeffrey Oyuela <jeffreysmac@Jeffreys-MacBook-Pro.local>
2025-05-03 20:44:34 -0400

    Added variables to index.py

--- backend/databaseStuff/src/index.py
+++ backend/databaseStuff/src/index.py
@@ -8,7 +8,14 @@ async def getConnection():
     current_dir = Path(__file__).parent
     env_path = current_dir.parent.parent / '.env'
     load_dotenv(dotenv_path=env_path)
-    conn = await asyncpg.connect(f"postgres://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_DATABASE")}?sslmode=require")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_DATABASE = os.getenv("DB_DATABASE")
+
+
+    conn = await asyncpg.connect(f"postgres://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}?sslmode=require")
 
     return conn
 
